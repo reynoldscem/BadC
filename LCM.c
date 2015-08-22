02b1618894bb1f25ca73fca21ef6f100261def48
Charlie Reynolds <reynoldscem@gmail.com>
2015-08-22 22:52:49 +0100

    LCM

---
+++ LCM.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define loop {max++;goto start;}
+#define output printf("%d\n",max);
+
+int main(void) {
+  int first, second, max;
+  printf("Enter two positive ints: ");
+  scanf("%d %d", &first, &second);
+  max = (first>second ? first : second);
+
+  start:
+    if (max % first || max % second) loop
+  output
+}
