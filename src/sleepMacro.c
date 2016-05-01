b84c23d0aa81dfc99a66586b1fdcb54c59089539
Charlie Reynolds <reynoldscem@gmail.com>
2016-05-01 14:34:12 +0100

    Made reusable within a given scope via advanced initialisation macro

--- src/sleepMacro.c
+++ src/sleepMacro.c
@@ -1,22 +1,26 @@
 #include <time.h>
-#include <stdio.h>
 
 #define ONLY_ONCE plsDontUseThisNameElseWhereOrIWontCompile
+#define INITIALISE int ONLY_ONCE;
 #define TIMEWAIT 357913941
-#define SPEED_LOOP(X) int ONLY_ONCE=0;for(;ONLY_ONCE<X;ONLY_ONCE++){asm volatile("nop");}
+#define SPEED_LOOP(X) ONLY_ONCE=0;for(;ONLY_ONCE<X;ONLY_ONCE++){asm volatile("nop");}
 #define SLEEP(X) SPEED_LOOP(TIMEWAIT*X) 
 #define PRODUCTION 1
 #ifndef PRODUCTION
   #include <limits.h>
+  #include <stdio.h>
   #define TIMING(X) X=(int)time(NULL);SPEED_LOOP(INT_MAX)X=INT_MAX/((int)time(NULL)-X);
 #endif
 int main(void) {
   #ifndef PRODUCTION
+    INITIALISE
     int i;
     TIMING(i)
     printf("Need %d loops per second ;)\n", i);
   #endif
   #ifdef PRODUCTION
+    INITIALISE
+    SLEEP(1)
     SLEEP(1)
   #endif
 }
